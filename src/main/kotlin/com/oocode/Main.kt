e0196b5b02809ccb01e21be165e264d2ef88d439
Ivan Moore <ivan@teamoptimization.com>
2023-12-01 11:05:49 +0000

    minor refactoring in preparation for q2

--- src/main/kotlin/com/oocode/Main.kt
+++ src/main/kotlin/com/oocode/Main.kt
@@ -2,11 +2,15 @@ import java.io.File
 import kotlin.text.Charsets.UTF_8
 
 fun main(args: Array<String>) {
-    println(q1(File(args[0]).readText(UTF_8)))
+    println(day1(File(args[0]).readText(UTF_8)))
 }
 
-fun q1(input: String) = input.split("\n").map { line -> line.calibrationValue() }.sum()
+fun day1(input: String) = input.split("\n").map { line -> line.calibrationValue() }.sum()
 
-fun String.calibrationValue() = (digits().first().digitToInt() * 10) + digits().last().digitToInt()
+fun String.calibrationValue() = (firstDigit() * 10) + lastDigit()
+
+private fun String.firstDigit() = digits().first().digitToInt()
+
+private fun String.lastDigit() = digits().last().digitToInt()
 
 fun String.digits() = filter { it.isDigit() }
--- src/test/kotlin/com/oocode/MainKtTest.kt
+++ src/test/kotlin/com/oocode/MainKtTest.kt
@@ -2,14 +2,14 @@ package com.oocode
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import day1
 import org.junit.jupiter.api.Test
-import q1
 
 internal class MainKtTest {
     @Test
     fun calculatesCorrectAnswerForExample() {
         assertThat(
-            q1(
+            day1(
                 """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
