7a978ece8804e47916b514aeeabb252ea417b429
Ivan Moore <ivan@teamoptimization.com>
2023-12-05 15:08:47 +0000

    day5 failing test work-in-progress

---
+++ src/main/kotlin/com/oocode/Almanac.kt
@@ -0,0 +1,11 @@
+package com.oocode
+
+fun almanacFrom(input: String): Almanac {
+    TODO("Not yet implemented")
+}
+
+class Almanac {
+    fun lowestLocationNumber(): Int {
+        TODO("Not yet implemented")
+    }
+}
