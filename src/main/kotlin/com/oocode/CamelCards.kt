2d9478142287457fb8412b4ecf024d9298c5a9d9
Ivan Moore <ivan@teamoptimization.com>
2023-12-07 15:51:30 +0000

    day 7 part 1 WIP - starting to implement comparison

--- src/main/kotlin/com/oocode/CamelCards.kt
+++ src/main/kotlin/com/oocode/CamelCards.kt
@@ -7,3 +7,18 @@ class CamelCardHands {
         TODO("Not yet implemented")
     }
 }
+
+data class CamelCardHand(val cards: String) {
+    enum class Type {
+        HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND
+    }
+
+    operator fun compareTo(that: CamelCardHand): Int {
+        return this.type().compareTo(that.type())
+    }
+
+    private fun type() =
+        if (cards.toSet().size == 1) Type.FIVE_OF_A_KIND else Type.HIGH_CARD
+}
+
+fun camelCardHandFrom(s: String) = CamelCardHand(s)
--- src/test/kotlin/com/oocode/Day7Test.kt
+++ src/test/kotlin/com/oocode/Day7Test.kt
@@ -2,6 +2,8 @@ package com.oocode
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
@@ -15,5 +17,27 @@ KK677 28
 KTJJT 220
 QQQJA 483"""
         assertThat(camelCardHandsFrom(input).totalWinnings(), equalTo(6440))
+    }
+
+    @Test
+    fun canRankHandsComparedToEachOther() {
+        val inWinningOrder = listOf(
+            "AAAAA" to "AA8AA"
+        )
+        for (pair in inWinningOrder) {
+            assertWinner(pair.first, pair.second)
+        }
+    }
+
+    private fun assertWinner(winner: String, loser: String) {
+        assertTrue(camelCardHandFrom(winner) > camelCardHandFrom(loser))
+        assertFalse(camelCardHandFrom(winner) < camelCardHandFrom(loser))
+        assertTrue(camelCardHandFrom(loser) < camelCardHandFrom(winner))
+        assertFalse(camelCardHandFrom(loser) > camelCardHandFrom(winner))
+
+        assertTrue(camelCardHandFrom(winner) != camelCardHandFrom(loser))
+        assertTrue(camelCardHandFrom(loser) != camelCardHandFrom(winner))
+        assertTrue(camelCardHandFrom(winner) == camelCardHandFrom(winner))
+        assertTrue(camelCardHandFrom(loser) == camelCardHandFrom(loser))
     }
 }
