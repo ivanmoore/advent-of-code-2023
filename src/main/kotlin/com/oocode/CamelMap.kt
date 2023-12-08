fbf087d5953c6b877c7a1097368226794fea439c
Ivan Moore <ivan@teamoptimization.com>
2023-12-08 18:06:59 +0000

    day 8 part 1 WIP - parsesInput

--- src/main/kotlin/com/oocode/CamelMap.kt
+++ src/main/kotlin/com/oocode/CamelMap.kt
@@ -1,9 +1,33 @@
 package com.oocode
 
-fun directionsFrom(input: String): CamelMap = TODO()
+fun directionsFrom(input: String): CamelMap {
+    val lines = input.split("\n")
+    val instructions = lines[0]
+        .map { if (it == 'L') Choice.Left else Choice.Right }
+    val nodes = lines.drop(2).map { nodeFrom(it) }
+    return CamelMap(instructions, nodes)
+}
 
-class CamelMap {
+enum class Choice { Left, Right }
+
+data class CamelMap(
+    private val instructions: List<Choice>,
+    private val nodes: List<Node>
+) {
     fun numberOfSteps(): Int {
-        TODO("Not yet implemented")
+        TODO()
     }
 }
+
+fun nodeFrom(line: String): Node = line
+    .replace("(", "")
+    .replace(")", "")
+    .split("=")
+    .let {
+        val name = it[0].trim()
+        val children = it[1].split(",")
+        return Node(name, Children(children[0].trim(), children[1].trim()))
+    }
+
+data class Children(val left: String, val right: String)
+data class Node(val name: String, val children: Children)
--- src/test/kotlin/com/oocode/Day8Test.kt
+++ src/test/kotlin/com/oocode/Day8Test.kt
@@ -2,6 +2,8 @@ package com.oocode
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import com.oocode.Choice.Left
+import com.oocode.Choice.Right
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
@@ -30,5 +32,22 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)"""
         assertThat(directionsFrom(input).numberOfSteps(), equalTo(6))
+    }
+
+    @Test
+    fun parsesInput() {
+        val input = """RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+ZZZ = (ZZZ, ZZZ)"""
+        assertThat(directionsFrom(input),
+            equalTo(CamelMap(
+                listOf(Right, Left),
+                listOf(
+                    Node("AAA", Children("BBB", "CCC")),
+                    Node("BBB", Children("DDD", "EEE")),
+                    Node("ZZZ", Children("ZZZ", "ZZZ")))
+            )))
     }
 }
