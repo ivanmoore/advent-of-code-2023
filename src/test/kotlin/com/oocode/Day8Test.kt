79412271fd957f5ff19c57d18a94069f8dceecc5
Ivan Moore <ivan@teamoptimization.com>
2023-12-08 18:35:00 +0000

    day 8 part 2 - WIP - failing test

--- src/test/kotlin/com/oocode/Day8Test.kt
+++ src/test/kotlin/com/oocode/Day8Test.kt
@@ -4,6 +4,7 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.oocode.Choice.Left
 import com.oocode.Choice.Right
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class Day8Test {
@@ -46,5 +47,21 @@ ZZZ = (ZZZ, ZZZ)"""
                     Node("BBB", Children("DDD", "EEE")),
                     Node("ZZZ", Children("ZZZ", "ZZZ")))
             )))
+    }
+
+    @Disabled
+    @Test
+    fun calculatesCorrectAnswersForGhostExample() {
+        val input = """LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"""
+        assertThat(directionsFrom(input).numberOfSteps(), equalTo(6))
     }
 }
